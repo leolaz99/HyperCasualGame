4be4a1a580adf6104d657bb8196dee6756471f4a
leolaz99 <leolaz99@gmail.com>
2021-01-18 16:11:03 +0100

    added player movement

---
+++ Assets/Scenes/Scripts/Boundaries.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2, 2), transform.position.y);
+    }
+}
---
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+   
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector2(transform.position.x + touch.deltaPosition.x * speed, transform.position.y);
+            }
+        }
+    }
+}
